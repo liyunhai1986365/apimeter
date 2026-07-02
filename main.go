6b84ae279108267f9b226acf242f88e0052784c8
CaIon <i@caion.me>
2026-07-02 21:58:41 +0800

    Make quota logging synchronous and delay startup log

--- main.go
+++ main.go
@@ -226,9 +226,6 @@ func main() {
 		port = strconv.Itoa(*common.Port)
 	}
 
-	// Log startup success message
-	common.LogStartupSuccess(startTime, port)
-
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: server,
@@ -239,6 +236,8 @@ func main() {
 			common.FatalLog("failed to start HTTP server: " + err.Error())
 		}
 	}()
+
+	common.LogStartupSuccess(startTime, port)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
