aec988f31837c3b64a481893660012826184e421
modelsell <jie.wag@gmail.com>
2026-07-18 00:07:59 +0800

    fix: restore startup middleware and traditional Chinese locale

--- main.go
+++ main.go
@@ -195,7 +195,7 @@ func main() {
 	// This will cause SSE not to work!!!
 	//server.Use(gzip.Gzip(gzip.DefaultCompression))
 	server.Use(middleware.RequestId())
-	server.Use(middleware.PoweredBy())
+	server.Use(middleware.Version())
 	server.Use(middleware.I18n())
 	middleware.SetUpLogger(server)
 	// Initialize session store
